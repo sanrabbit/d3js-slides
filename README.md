a7608b360746c4f3dd748d7360e64916656e7a96
hueidou <hueidou163@gmail.com>
2014-05-03 22:37:53 +0800

    hjkl~

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-d3js-slides
+D3.js Data-Driven Documents
 ===========
 
 * Why D3.js / 数据可视化
 * Why D3.js / Web图表工具优缺点对比
+* What D3.js / 简介
+* How D3.js / API介绍
+* How D3.js / 示例
+* 其他
---
+++ practice/d3api-tree/getdate.js
@@ -0,0 +1,27 @@
+﻿var list = $('.markdown-body').children();
+list.splice(0, 5);
+
+var d3api = { name: 'd3api', children: [], href: 'https://github.com/mbostock/d3/wiki/API-Reference' };
+
+list.each(function () {
+    switch (this.tagName) {
+        case 'H2':
+            h2 = { name: this.children[1].innerText, href: this.children[1].href, children: [] };
+            d3api.children.push(h2);
+            break;
+        case 'H3':
+            h3 = { name: this.children[1].innerText, href: this.children[1].href, children: [] };
+            h2.children.push(h3);
+            break;
+        case 'UL':
+            $('a', this).each(function () {
+                var a = { name: this.innerText, href: this.href };
+                h3.children.push(a);
+            });
+            break;
+        default:
+            break;
+    }
+});
+
+JSON.stringify(d3api, undefined, '  ');
